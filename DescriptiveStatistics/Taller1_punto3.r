98a114e8b962d84fb074ebd5980726bfb70c7510
alejorodasvasquez <alejorodasvasquez@gmail.com>
2018-06-29 07:31:05 -0500

    Nuevos archivos

--- DescriptiveStatistics/Taller1_punto3.r
+++ DescriptiveStatistics/Taller1_punto3.r
@@ -15,12 +15,13 @@ abline(v=moda, col = "red", lty = 2)
 
 # Plot
 plot(UrbanPop,Assault,main="UrbanPop vs Assault")
-abline(lm(Assault~UrbanPop),col=4)
-points(UrbanPop[Assault >= 100 & Assault <= 150], 
-       Assault[Assault >= 100 & Assault <= 150],col="green",pch=20 )
-points(UrbanPop[Assault >= 225 & Assault <= 275], 
-       Assault[Assault >= 225 & Assault <= 275],col="red",pch=20 )
+population.median <- median(UrbanPop)
 
+abline(lm(Assault~UrbanPop),col=4)
+lines(lowess(UrbanPop,Assault),col="red",lwd=2,lty=2)
+
+points(UrbanPop[UrbanPop >= population.median],
+       Assault[UrbanPop >= population.median],col="green",pch=20 )
 plot.new()
-legend("top", c("Asaltos entre 100 y 150","Asaltos entre 225 y 275"),
+legend("top", paste(c("Media poblacional igual o superior a "),population.median),
        pch=c(16, 16), col=c("green", "red"))
--- IntermediateGraphs/ScatterPlot/scatterPlot.r
+++ IntermediateGraphs/ScatterPlot/scatterPlot.r
@@ -7,13 +7,13 @@ abline(lm(mpg~wt), col="red", lwd=2, lty=1)
 lines(lowess(wt,mpg), col="blue", lwd=2, lty=2)
 
 
-library(car)
-scatterplot(mpg ~ wt | cyl, data=mtcars, lwd=2,
-            main="Scatter Plot of MPG vs. Weight by # Cylinders",
-            xlab="Weight of Car (lbs/1000)",
-            ylab="Miles Per Gallon",
-            legend.plot=TRUE,
-            id.method="identify",
-            labels=row.names(mtcars),
-            boxplots="xy"
-)
+# library(car)
+# scatterplot(mpg ~ wt | cyl, data=mtcars, lwd=2,
+#             main="Scatter Plot of MPG vs. Weight by # Cylinders",
+#             xlab="Weight of Car (lbs/1000)",
+#             ylab="Miles Per Gallon",
+#             legend.plot=TRUE,
+#             id.method="identify",
+#             labels=row.names(mtcars),
+#             boxplots="xy"
+# )
