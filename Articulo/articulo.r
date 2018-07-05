e555445eeca6d139784c9c54897e711406106f1c
alejo <alejorodasvasquez@gmail.com>
2018-07-05 05:03:01 -0500

    archivos relacionados con el articulo de investigacion

---
+++ Articulo/articulo.r
@@ -0,0 +1,8 @@
+mydata <- read.table("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/Hurto_1.csv", 
+                      header = T, sep= ";",
+                     encoding = "UTF-8", stringsAsFactors = TRUE,
+                     fill = TRUE)g
+
+mydata.tsv <- read.delim("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/Hurto.tsv",
+                         )
+
