1869374513c40d93dd70a89d3a1e9e2726819b8f
alejorodasvasquez <alejorodasvasquez@gmail.com>
2018-07-06 10:52:15 -0500

    se agregan lista de actvidades

--- Articulo/articulo.r
+++ Articulo/articulo.r
@@ -1,8 +1,10 @@
-mydata <- read.table("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/Hurto_1.csv", 
+mydata <- read.table("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/Hurto_2.csv", 
                       header = T, sep= ";",
-                     encoding = "UTF-8", stringsAsFactors = TRUE,
-                     fill = TRUE)g
+                     encoding = "UTF-8", 
+                     fill = TRUE)
 
 mydata.tsv <- read.delim("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/Hurto.tsv",
                          )
 
+# Seleccionar las variables que seran objeto de estudio
+# Crear la exploración gráfica de variables perdidas y la summary
