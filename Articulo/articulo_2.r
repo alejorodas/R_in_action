6af3839d9ab227de171919ee139ec0542e1b80a2
alejorodasvasquez <alejorodasvasquez@gmail.com>
2018-07-23 16:47:25 -0500

    se refactoriza la funcion format.Hora y se crea el procedimiento binding para traducir la hora en que se comete el hurto a la categoria Madrugada,Mañana,Tarde,Noche

--- Articulo/articulo_2.r
+++ Articulo/articulo_2.r
@@ -1,17 +1,16 @@
 load(file ="/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/data.Rda")
 
-# f<-format(data.set.hurto$Hora[3], format = "%m-%d-%Y %I:%M:%S %p")
-# dy <- as.POSIXct(f, format = "%m/%d/%Y %I:%M:%S %p")
-# strsplit(as.character(d), " ")[[1]][2]
-
 format.Hora <- function(dataset) {
   Hora <- format(dataset$Hora, format = "%m-%d-%Y %I:%M:%S %p")
   d <- as.POSIXct(Hora, format = "%m/%d/%Y %I:%M:%S %p")
-  dataset$hora.hurto <- substr(d, 12, 19)
-  #dataset$hora.hurto <- format(d, "%I:%M:%S")
+  dataset$hora.hurto <- times(substr(d, 12, 19))
   return(dataset)
 }
 
 dataset <- format.Hora(data.set.hurto)
+
+#Binding numerical - categorical
 time.tag <- chron(times=c('00:00:00','06:00:00','12:00:00','18:00:00','23:59:00'))
-hurto.inverval <- cut(dataset$hora.hurto,breaks = time.tag, labels = c("Night","Morning","Afernoon","Evening"),include.lowest = TRUE)
+hurto.inverval <- cut(dataset$hora.hurto,breaks = time.tag, labels = c("Madrugada","Mañana","Tarde","Noche"),include.lowest = TRUE)
+
+table(hurto.inverval)
