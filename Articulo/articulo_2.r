57333a51ed66cebe05b676ea5f34ad3d732e6d7f
alejo <alejorodasvasquez@gmail.com>
2018-07-22 21:32:49 -0500

    se crea metodo para formatear la hora del crimen convirtiendola en el formato de 24 horas

--- Articulo/articulo_2.r
+++ Articulo/articulo_2.r
@@ -1,2 +1,13 @@
 load(file ="/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/Articulo/data.Rda")
-data.set.hurto
+
+# f<-format(data.set.hurto$Hora[3], format = "%m-%d-%Y %I:%M:%S %p")
+# dy <- as.POSIXct(f, format = "%m/%d/%Y %I:%M:%S %p")
+# strsplit(as.character(d), " ")[[1]][2]
+
+format.Hora <- function(dataset) {
+  Hora <- format(dataset$Hora, format = "%m-%d-%Y %I:%M:%S %p")
+  dataset$hora.hurto <- as.POSIXct(Hora, format = "%m/%d/%Y %I:%M:%S %p")
+  return(dataset)
+}
+
+dataset <- format.Hora(data.set.hurto)
