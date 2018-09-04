d6a59543a29a57ace6480623b332853c00f69460
alejo <alejorodasvasquez@gmail.com>
2018-09-03 21:38:50 -0500

    sample para aprender spatial

---
+++ SpatialAnalysis/spatial_sample.r
@@ -0,0 +1,11 @@
+library(rgdal)
+library(tmap)
+library(leaflet)
+Output.Areas <- readOGR("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/SpatialAnalysis/Camden_oa11/Camden_oa11.shp")
+#plot(Output.Areas)
+
+Census.Data <-read.csv("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/SpatialAnalysis/practicaldata.csv")
+
+head(Output.Areas@data)
+OA.Census <- merge(Output.Areas, Census.Data, by.x="OA11CD", by.y="OA")
+plot(OA.Census)
