64e8fc3c1e766f3fdf149a712b9ead1e41f68dc8
alejo <alejorodasvasquez@gmail.com>
2018-09-06 08:37:55 -0500

    se realizan ejemplos de analisis espacial

--- SpatialAnalysis/spatial_sample.r
+++ SpatialAnalysis/spatial_sample.r
@@ -6,8 +6,17 @@ Output.Areas <- readOGR("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R
 
 Census.Data <-read.csv("/Users/alejandrorodas/Documents/Libros/R/R_ejercicios/R_in_action/SpatialAnalysis/practicaldata.csv")
 
-head(Output.Areas@data)
+#head(Output.Areas@data)
 OA.Census <- merge(Output.Areas, Census.Data, by.x="OA11CD", by.y="OA")
-plot(OA.Census)
+#plot(OA.Census)
 
-qtm(OA.Census, fill = "Qualification")
+tm_shape(OA.Census) + 
+  
+tm_fill("Qualification", palette = "Greens", style = "quantile", n = 5,
+         title = "% with a Qualification") + 
+
+tm_compass() +
+
+tm_layout(title = "Camden, London", legend.text.size = 1.1,
+          legend.title.size = 1.4, legend.position = c("right", "top"), 
+          frame = FALSE)
