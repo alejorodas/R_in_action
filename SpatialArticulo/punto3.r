a5adcfac378bec9aaea9f11e4d988ebba9fcd00e
alejo <alejorodasvasquez@gmail.com>
2018-10-06 18:03:39 -0500

    se cambia escala de colores

--- SpatialArticulo/punto3.r
+++ SpatialArticulo/punto3.r
@@ -8,9 +8,10 @@ temp$dia <- factor(temp$dia, order=TRUE,levels=c("Lunes", "Martes", "Miercoles",
 
 
 ggplot(temp, aes(x= temp$dia, y= factor(temp$intervalo))) + geom_tile(aes(fill =  count)) +
-  scale_x_discrete("Dia de la Semana", expand = c(0,0)) +
+  scale_x_discrete("D??a de la Semana", expand = c(0,0)) +
   scale_y_discrete("Hora del Dia", expand = c(0,-2)) + 
-  scale_fill_gradient("Cantidad de Hurtos", low = "white", high = "steelblue") +
-  theme_bw() + ggtitle("Hurtos por Dia y Hora en la Semana") + theme(plot.title = element_text(hjust = 0.5))
+  scale_fill_gradient("Cantidad de Hurtos", low = "#56B1F7", high = "#132B43", space = "Lab",
+                      na.value = "grey50") +
+  theme_bw() + ggtitle("Hurtos por D??a y Hora en la Semana") + theme(plot.title = element_text(hjust = 0.5)) +
   theme(panel.grid.major = element_line(colour = NA), panel.grid.minor = element_line(colour = NA))
 
